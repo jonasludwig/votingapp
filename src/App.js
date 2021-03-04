adf09bd9cd79f9891168bca704665caea9ade756
Jonas Ludwig <jonasludwig1998@gmail.com>
2021-03-04 15:57:08 +0100

    remove react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import './App.css';
 import "@fontsource/roboto";
-import MyWebcam from "./components/MyWebcam";
-import WelcomeScreen from "./components/WelcomeScreen";
-import ReturnCode from "./components/ReturnCode";
-import VotingCode from "./components/VotingCode";
-import FinalisationCode from "./components/FinalisationCode";
-import ConfirmationCode from "./components/ConfirmationCode";
-import EndScreen from "./components/EndScreen";
-import {BrowserRouter as Router, Route} from "react-router-dom";
 import {CssBaseline, Grid} from "@material-ui/core";
 import Topnav from "./components/Topnav";
-import Botnav from "./components/Botnav";
 import TestStepper from "./components/TestStepper";
 
 function App() {
