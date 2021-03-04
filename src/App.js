69bb79261a231549fc86233185d036ee438cf77a
Jonas Ludwig <jonasludwig1998@gmail.com>
2021-03-04 15:42:27 +0100

    add gh-pages setup

--- src/App.js
+++ src/App.js
@@ -7,24 +7,35 @@ import VotingCode from "./components/VotingCode";
 import FinalisationCode from "./components/FinalisationCode";
 import ConfirmationCode from "./components/ConfirmationCode";
 import EndScreen from "./components/EndScreen";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {CssBaseline, Grid} from "@material-ui/core";
+import Topnav from "./components/Topnav";
+import Botnav from "./components/Botnav";
+import TestStepper from "./components/TestStepper";
 
 function App() {
 
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <Router>
-                    <Route path="/" exact component={WelcomeScreen}/>
-                    <Route path="/webcam" component={MyWebcam}/>
-                    <Route path="/votingcode" component={VotingCode}/>
-                    <Route path="/returncode" component={ReturnCode}/>
-                    <Route path="/confirmationcode" component={ConfirmationCode}/>
-                    <Route path="/finalisationcode" component={FinalisationCode}/>
-                    <Route path="/endscreen" component={EndScreen}/>
-                </Router>
-            </header>
+        <div>
+            <Grid container direction="column">
+                <Topnav className="Topnav"/>
+                <div className="ContainerStyles">
+                    <Router>
+                        <Route path="/" exact component={WelcomeScreen}/>
+                        <Route path="/webcam" component={MyWebcam}/>
+                        <Route path="/votingcode" component={VotingCode}/>
+                        <Route path="/returncode" component={ReturnCode}/>
+                        <Route path="/confirmationcode" component={ConfirmationCode}/>
+                        <Route path="/finalisationcode" component={FinalisationCode}/>
+                        <Route path="/endscreen" component={EndScreen}/>
+                        <Route path="/test" component={TestStepper}/>
+                    </Router>
+                </div>
+                {/*<Botnav/>*/}
+            </Grid>
+            <CssBaseline/>
+
         </div>
     );
 }
---
+++ src/components/Topnav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {AppBar, Toolbar} from "@material-ui/core";
+
+function Topnav(props) {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <h1>Wahlapp</h1>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Topnav;
