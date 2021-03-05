5a8a90d15e0a3ecb82c6263054ade1e8b4fde02a
Jonas Ludwig <jonasludwig1998@gmail.com>
2021-03-05 10:15:43 +0100

    use outwards facing camera

--- src/components/MyWebcam.js
+++ src/components/MyWebcam.js
@@ -5,11 +5,11 @@ function MyWebcam(props) {
     const videoConstraints = {
         width: { min: 480 },
         height: { min: 720 },
+        facingMode: { exact: "environment" }
     };
 
     return (
         <Webcam videoConstraints={videoConstraints}
-                width={480}
                 height={720}/>
     );
 }
